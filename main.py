f03ca6acdc0b5cd412283bae675604e8f7385dbd
Ludovic Legrand <ludovic.legrand@gmail.com>
2017-10-30 22:08:09 -0700

    Wrapping service for Nate-ODR

--- main.py
+++ main.py
@@ -12,13 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-# from __future__ import print_function # In python 2.7
-import sys
-
 # [START app]
 import logging
 
+import numpy as np
 
 import os
 import phaselist
@@ -26,19 +23,11 @@ import phaselist
 import StringIO
 import csv
 
-from logics import QuantModeModel
-
-from flask import Flask, flash, request, render_template, redirect, session, make_response, url_for, send_from_directory
+from flask import Flask, request, render_template, session, make_response
 
 #Application modules
 import qxrd
 import qxrdtools
-
-import samples
-import json
-
-from werkzeug.utils import secure_filename
-
 
 UPLOAD_DIR = 'uploads'
 UPLOAD_FOLDER = UPLOAD_DIR
@@ -55,143 +44,17 @@ app.secret_key = 'Ludo'
 
 app.config['DEBUG'] = True
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-# This will upload a file and print content
-@app.route('/boo', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
-
-
-@app.route('/printdata', methods=['GET', 'POST'])
-def printdata():
-    resp = make_response(render_template('index.html'))
-    return resp    
-
-@app.route('/odr_demo')
-def odr_demo():
-    return render_template('odr_demo.html')
+@app.route('/')
+def hello():
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/')
-def hello():
-    session['inventory'] = 'rockforming';
-    session['lambda'] = 0
-    session['target'] = "Co"
-    session['fwhma'] = -0.001348
-    session['fwhmb'] = 0.352021
-    session['title'] = 'default'
-    session['selected'] = phaselist.defaultPhases
-    session['available'] = phaselist.availablePhases
-
-    # print(session, file=sys.stderr)
-    return render_template('index.html')
-
-@app.route('/run_post')
-def run_post():
-    url = '/parse_post'
-    data = {'name': 'Burbank', 'format': 'XRD', 'coord': 36}
-    headers = {'Content-Type': 'application/json'}
-
-    return Request('POST',url, data=json.dumps(data), headers=headers)
-
-@app.route('/parse_post', methods=['POST'])
-def parse_post():
-    if request.method == 'POST':
-        json_dict = request.get_json()
-        filename = json_dict['name']
-#        print(filename, file=sys.stderr)
-        print(filename)
-
-        return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>YES!</h1>
-    '''
-    else:
-        return '''<html><body>Something went horribly wrong</body></html>'''
-
-@app.route('/cheminfile')
-def get_data():
-#    return("upload your file")
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
-    
-# [START form]
-# This will upload a file and process as quant
-@app.route('/form')
-def form():
-    return """
-<form method="POST" action="/process" enctype="multipart/form-data">
-    <input type="file" name="file">
-    <input type="submit">
-</form>
-"""
-# [END form]
-
-# [START phase setting]
-@app.route('/phase', methods=['GET', 'POST'])
-def phase():
-    if request.method == 'POST':
-        selectedlist = request.form['selectedphase']
-        availlist = request.form['availablephase']
-        # selectedlist.sort()
-        # availlist.sort()
-
-        return redirect('/')
-    else:
-        mode = QuantModeModel()
-        # print(mode, file=sys.stderr)
-        # session['mode'] = mode
-
-        template_vars = {
-            'availablephaselist': mode.available,
-            'selectedphaselist': mode.selected,
-            'mode': ''
-        }
-        return render_template('phase.html', **template_vars)
+@app.route('/odr_demo')
+def odr_demo():
+    return render_template('odr_demo.html')
 
 # [START process]
 @app.route('/process', methods=['POST'])
@@ -206,29 +69,13 @@ def process():
     filename = uploaded_file.filename
     XRDdata = uploaded_file
     userData = qxrdtools.openXRD(XRDdata, filename)
-
-    # Unpack the selected inventory
-    if session['inventory'] == "cement":
-        # phaselistname = 'difdata_cement_inventory.csv'
-        DBname ='difdata_cement.txt'
-    elif session['inventory'] == "pigment":
-        # phaselistname = 'difdata_pigment_inventory.csv'
-        DBname ='difdata_pigment.txt'
-    elif session['inventory'] == "rockforming":
-        # phaselistname = 'difdata-rockforming_inventory.csv'
-        DBname ='difdata-rockforming.txt'
-    elif session['inventory'] == "chemin":
-        # phaselistname = 'difdata_CheMin_inventory.csv'
-        DBname ='difdata_CheMin.txt'
-    else:
-        logging.critical("Can't find inventory")
-    # datafilename = "Mix3C-film.txt"
+    # print userData
+    DBname ='difdata_CheMin.txt'
     
-    # Calibration parameters
-    Lambda = session['lambda']
-    Target = session['target']
-    FWHMa = session['fwhma']
-    FWHMb = session['fwhmb']
+    Lambda = 0.0
+    Target = 'Co'
+    FWHMa = 0.0
+    FWHMb = 0.35
 
     # Boundaries check
     if(Lambda > 2.2 or Lambda == 0):
@@ -245,7 +92,9 @@ def process():
     InstrParams = {"Lambda": Lambda, "Target": Target, "FWHMa": FWHMa, "FWHMb": FWHMb}
 
     # Phase selection
-    selectedPhases = session['selected']
+    #    selectedPhases = session['selected']
+    selectedPhases = phaselist.defaultPhases
+    # print selectedPhases
     # print(selectedPhases, file=sys.stderr)
 
     # Dif data captures all cristallographic data
@@ -255,6 +104,8 @@ def process():
         code = int(code)
         selectedphases.append((name,code))
 
+    # print selectedphases
+        
     # Load in the DB file
     difdata = open(DBname, 'r').readlines()
 
@@ -263,8 +114,6 @@ def process():
     # print(twoT.tolist(), file=sys.stderr)
     # print(userData, file=sys.stderr)
 
-    # session.results = results
-    # session.put()
     twoT = userData[0]
     diff = userData[1]
 
@@ -275,10 +124,10 @@ def process():
     # diff = diff
     bgpoly = BG
     #calcdiff = calcdiff
-
-
     # csv = session_data_key.urlsafe()
-    csv = 'LUDO'
+    csv = 'ODR'
+    session['results'] = results
+    session['filename'] = filename
     
     template_vars = {
         'phaselist': results,
@@ -289,220 +138,84 @@ def process():
         'url_text': csv,
         'key': 'ludo',
         'samplename': filename,
-        'mode': ''
+        'mode': 'Chemin-ODR'
     }
     return render_template('chart.html', **template_vars)
-
-    
-    #    return "Total computation  time = %.2fs" %(time.time()-t0)
-    #return_str = ''
-    #return_str += 'results: {}<br />'.format(str(results))
-    #return return_str
 # [END process]
 
-# [START parseSampleFile]
-@app.route('/parseSampleFile', methods=['POST'])
-def parse_sample_file():
-    uploaded_file = request.files.get('file')
-    if not uploaded_file:
-        return 'No file uploaded.', 400
-
-    # Load the sample data file in userData
-    # parse sample data file wrt format
-
-    filename = uploaded_file.filename
-    XRDdata = uploaded_file
-    userData = qxrdtools.openXRD(XRDdata, filename)
-    
-    return render_template('chart.html', **template_vars)
-
-    
-    #    return "Total computation  time = %.2fs" %(time.time()-t0)
-    #return_str = ''
-    #return_str += 'results: {}<br />'.format(str(results))
-    #return return_str
-# [END parseSampleFile]
-
-# [START chemin micro service]
-# WE are not using np.loadtxt to split angle, diff so no need to massage for display
-# util for chemin service
-# util for chemin service
-# util for chemin service
-@app.route('/odr_process', methods=['POST'])
-def odr_process_data():
-    json_dict = request.get_json()
-    # samplename = json_dict['samplename']
-    # list = json_dict['phaselist']
-    #    selectedphases = json_dict.items()
-    list = json_dict['phaselist']
-    samplename = json_dict['samplename']
-    angle = json_dict['angle']
-    diff = json_dict['diff']
-    selectedphases = list.items()
-    # print(selectedphases, file=sys.stderr)
-
-    # Chemin: Parameters are fixed, as well as the mineral database
-    InstrParams = {"Lambda": 0, "Target": 'Co', "FWHMa": 0.00, "FWHMb": 0.35}
-    DBname ='difdata_CheMin.txt'
-    # Dif data captures all cristallographic data
-    # Load in the DB file
-    difdata = open(DBname, 'r').readlines()
-    
-    #Data coming from ODR
-    #samplename = "Mix3C-film.txt"
-    # userData = (samples.angle, samples.diff)
-    userData = (angle, diff)
-#    selectedphases = samples.phaselist.items()
-    # print(selectedphases, file=sys.stderr)
-        
-    results, BG, calcdiff = qxrd.Qanalyze(userData, difdata, selectedphases, InstrParams)
-    # print(userData, file=sys.stderr)
-
-    twoT = userData[0]
-    diff = userData[1]
-
-    # logging.debug(results)
-    # logging.info("Done with processing")
-
-    angle = twoT
-    # diff = diff
-    bgpoly = BG
-    #calcdiff = calcdiff
-
-    # csv = session_data_key.urlsafe()
-    csv = 'LUDO'
-    
-    template_vars = {
-        'phaselist': results,
-        'angle': angle,
-        'diff': diff,
-        'bgpoly': bgpoly.tolist(),
-        'sum': calcdiff.tolist(),
-        'url_text': csv,
-        'key': 'ludo',
-        'samplename': samplename,
-        'mode': ''
-    }
-    return render_template('chart.html', **template_vars)
-# [END chemin]
-
-# [START CVS]
-@app.route('/csvDownload', methods=['GET'])
-def csvDownload():
-#    response.headers['Content-Type'] = 'text/csv'
-#    response.headers['Content-Disposition'] = 'attachment; filename={}.csv'.format(session.sampleFilename)
-#    writer = csv.writer(self.response.out)
-#    writer.writerow(['Mineral', 'AMCSD', 'Mass %'])
-#    writer.writerows(session.results)
-
-    line = StringIO.StringIO()
-    cw = csv.writer(line)
-    cw.writerow(['Mineral', 'AMCSD', 'Mass %'])
-    output = make_response(line.getvalue())
-    output.headers["Content-Disposition"] = "attachment; filename=export.csv"
-    output.headers["Content-type"] = "text/csv"
-    return output
-# [END CVS]
-
-
-# Start ludo
-@app.route('/ludo')
-def ludo():
-    return render_template('odr.html')
-
-@app.route('/odr', methods=['POST'])
+# [START ODR service]
+@app.route('/odr', methods=['GET','POST'])
 def odr():
     if request.method == 'POST':
-        json_dict = request.get_json()
-        sample = json_dict["sample"]
-        samplename = sample["name"]
-
-        array = sample["data"]
-        angle = [li['x'] for li in array]
-        diff = [li['y'] for li in array]
+        #L oad data from request
+        json_data = request.get_json()
         
-        phasearray = json_dict["phases"]
+        data = json_data
+        sample = data['sample']
+        filename = sample['name']
+        array = sample['data']
+
+        x = [li['x'] for li in array]
+        y = [li['y'] for li in array]
+
+        angle = np.asfarray(np.array(x))
+        diff = np.asfarray(np.array(y))
+
+        phasearray = data['phases']
         selectedphases = [(d['name'], d['AMCSD_code']) for d in phasearray]
-
-        # print(selectedphases, file=sys.stderr)
-        # print(samplename, file=sys.stderr)
-        # print(angle, file=sys.stderr)
-        # print(diff, file=sys.stderr)
-
         InstrParams = {"Lambda": 0, "Target": 'Co', "FWHMa": 0.00, "FWHMb": 0.35}
         DBname ='difdata_CheMin.txt'
         # Dif data captures all cristallographic data
         # Load in the DB file
         difdata = open(DBname, 'r').readlines()
         userData = (angle, diff)
-        print(userData)
-        
+        # print(userData)
         results, BG, calcdiff = qxrd.Qanalyze(userData, difdata, selectedphases, InstrParams)
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>GOOD</h1>
-    '''
 
-@app.route('/ludo', methods=['POST'])
-def ludo_process_data():
-    json_dict = request.get_json()
+        twoT = userData[0]
+        diff = userData[1]
 
-    sample = json_dict["sample"]
-    samplename = sample["name"]
+        # logging.debug(results)
+        # logging.info("Done with processing")
 
-    array = sample["data"]
-    angle = [li['x'] for li in array]
-    diff = [li['y'] for li in array]
+        angle = twoT
+        # diff = diff
+        bgpoly = BG
+        #calcdiff = calcdiff
 
-    phasearray = json_dict["phases"]
-    selectedphases = [(d['name'], d['AMCSD_code']) for d in phasearray]
-
-    print(selectedphases)
-
-    # Chemin: Parameters are fixed, as well as the mineral database
-    InstrParams = {"Lambda": 0, "Target": 'Co', "FWHMa": 0.00, "FWHMb": 0.35}
-    DBname ='difdata_CheMin.txt'
-    # Dif data captures all cristallographic data
-    # Load in the DB file
-    difdata = open(DBname, 'r').readlines()
-    
-    #Data coming from ODR
-    #samplename = "Mix3C-film.txt"
-    # userData = (samples.angle, samples.diff)
-    userData = (angle, diff)
-    print(userData)
-    #    selectedphases = samples.phaselist.items()
+        # csv = session_data_key.urlsafe()
+        csv = 'ODR'
+        session['results'] = results
+        session['filename'] = filename
         
-    results, BG, calcdiff = qxrd.Qanalyze(userData, difdata, selectedphases, InstrParams)
+        template_vars = {
+            'phaselist': results,
+            'angle': angle.tolist(),
+            'diff': diff.tolist(),
+            'bgpoly': bgpoly.tolist(),
+            'sum': calcdiff.tolist(),
+            'url_text': csv,
+            'key': 'ludo',
+            'samplename': filename,
+            'mode': 'Chemin-ODR'
+        }
+        return render_template('chart.html', **template_vars)
+    else:
+        return '''<html><body><h1>Did not get a post!</h1></body></html>'''
+# [END ODR service]
 
-    twoT = userData[0]
-    diff = userData[1]
-
-    # logging.debug(results)
-    # logging.info("Done with processing")
-
-    angle = twoT
-    # diff = diff
-    bgpoly = BG
-    #calcdiff = calcdiff
-
-    # csv = session_data_key.urlsafe()
-    csv = 'LUDO'
-    
-    template_vars = {
-        'phaselist': results,
-        'angle': angle,
-        'diff': diff,
-        'bgpoly': bgpoly.tolist(),
-        'sum': calcdiff.tolist(),
-        'url_text': csv,
-        'key': 'ludo',
-        'samplename': samplename,
-        'mode': ''
-    }
-    return render_template('chart.html', **template_vars)
-# [END ludo]
+# [START CVS]
+@app.route('/csvDownload', methods=['GET'])
+def csvDownload():
+    line = StringIO.StringIO()
+    cw = csv.writer(line)
+    cw.writerow(['Mineral', 'AMCSD', 'Mass %'])
+    cw.writerows(session['results'])
+    output = make_response(line.getvalue())
+    output.headers["Content-Disposition"] = "attachment; filename={}.csv".format(session['filename'])
+    output.headers["Content-type"] = "text/csv"
+    return output
+# [END CVS]
 
 @app.errorhandler(500)
 def server_error(e):
